85a847595a6713a9e62a7ee64ddbe8ea235a0b85
eugeniosant <eugenio.santoboni@gmail.com>
2013-03-14 19:04:14 +0100

    Upgrade default resources

--- src/main/webapp/protected/databaseBackups/develop/servDataSource/authusers.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO authusers (username, passwd, registrationdate, lastaccess, lastpasswordchange, active) VALUES ('admin', 'adminadmin', '2008-10-10 00:00:00', '2013-03-14 00:00:00', NULL, 1);
