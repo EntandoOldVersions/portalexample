bf2bc2e5192cf0485296713e04c4038d7bef6965
William Ghelfi <w.ghelfi@entando.com>
2013-10-10 17:37:11 +0200

    add default profile to admin

--- src/main/resources/sql/portalexampleServ_data.sql
+++ src/main/resources/sql/portalexampleServ_data.sql
@@ -25,6 +25,9 @@ INSERT INTO authusergroups (username, groupname) VALUES ('admin', 'administrator
 
 INSERT INTO authuserroles (username, rolename) VALUES ('admin', 'admin');
 
+INSERT INTO authuserprofiles (username, profiletype, profilexml, publicprofile) VALUES ('admin', 'PFL', '<?xml version="1.0" encoding="UTF-8"?>
+<profile id="admin" typecode="PFL" typedescr="Default user profile"><descr /><groups /><categories /><attributes><attribute name="fullname" attributetype="Monotext" /><attribute name="email" attributetype="Monotext" /></attributes></profile>
+', 0);
 
 INSERT INTO authusershortcuts (username, config) VALUES ('admin', '<?xml version="1.0" encoding="UTF-8"?>
 <shortcuts>
