39afff57cc85676f711ea1a7dfcda55ea6f25e16
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2014-05-02 16:44:30 +0200

    Upgrade fragment scripts

--- src/main/webapp/protected/databaseBackups/develop/portDataSource/guifragment.sql
+++ src/main/webapp/protected/databaseBackups/develop/portDataSource/guifragment.sql
@@ -1,8 +1,8 @@
-INSERT INTO guifragment (code, widgettypecode, plugincode, gui) VALUES ('content_viewer', 'content_viewer', 'jacms', '<#assign jacms=JspTaglibs["/jacms-aps-core"]>
-<@jacms.content publishExtraTitle=true />');
-INSERT INTO guifragment (code, widgettypecode, plugincode, gui) VALUES ('entando-widget-search_form', 'entando-widget-search_form', NULL, '<#assign wp=JspTaglibs["/aps-core"]>
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('content_viewer', 'content_viewer', 'jacms', '<#assign jacms=JspTaglibs["/jacms-aps-core"]>
+<@jacms.content publishExtraTitle=true />', NULL, 0);
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-search_form', 'entando-widget-search_form', NULL, '<#assign wp=JspTaglibs["/aps-core"]>
 
 <@wp.pageWithWidget var="searchResultPageVar" widgetTypeCode="search_result" />
 <form class="navbar-search pull-left" action="<@wp.url page="${searchResultPageVar.code}" />" method="get">
 <input type="text" name="search" class="search-query span2" placeholder="Give me a word for Matteo" x-webkit-speech="x-webkit-speech" />
-</form> ');
+</form> ', NULL, 0);
