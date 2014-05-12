6ce22234195b4388a48c3fbbda231932bd65ca95
eugeniosant <eugenio.santoboni@gmail.com>
2014-05-12 18:29:24 +0200

    upgrade Gui Fragment scripts

--- src/main/webapp/protected/databaseBackups/develop/portDataSource/guifragment.sql
+++ src/main/webapp/protected/databaseBackups/develop/portDataSource/guifragment.sql
@@ -1,11 +1,10 @@
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('content_viewer', 'content_viewer', 'jacms', '<#assign jacms=JspTaglibs["/jacms-aps-core"]>
 <@jacms.content publishExtraTitle=true />', NULL, 0);
-INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-search_form', 'entando-widget-search_form', NULL, '<#assign wp=JspTaglibs["/aps-core"]>
-
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('entando-widget-search_form', 'entando-widget-search_form', NULL, NULL, '<#assign wp=JspTaglibs["/aps-core"]>
 <@wp.pageWithWidget var="searchResultPageVar" widgetTypeCode="search_result" />
 <form class="navbar-search pull-left" action="<@wp.url page="${searchResultPageVar.code}" />" method="get">
-<input type="text" name="search" class="search-query span2" placeholder="Give me a word for Matteo" x-webkit-speech="x-webkit-speech" />
-</form> ', NULL, 0);
+<input type="text" name="search" class="search-query span2" placeholder="<@wp.i18n key="ESSF_SEARCH" />" x-webkit-speech="x-webkit-speech" />
+</form> ', 1);
 INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('models-common-utils', NULL, NULL, '<#assign wp=JspTaglibs["/aps-core"]>
 <#-- css -->
 <@wp.outputHeadInfo type="CSS">
